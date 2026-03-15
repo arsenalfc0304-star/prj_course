e5b7f47448da56240def8fe1d88f2ed53ad2c1cb
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-03-15 20:49:06 +0500

    cw in progress

--- src/reports.py
+++ src/reports.py
@@ -5,10 +5,13 @@ from utils import read_data_from_excel
 
 
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
-    """ """
-    pass
+    """
+    :param transactions:
+    """
+    if datetime.datetime.strptime(transaction["Дата операции"][3:5], "%m) > 11:
+
+    date_filtered_transactions = transactions.loc[transactions['Дата операции']
+    return transactions.loc[transactions.Категория.isin([category])]
 
 
-
-
-spending_by_category(read_data_from_excel("data/operations.xlsx"))
+print(spending_by_category(read_data_from_excel("data/operations.xlsx"), 'Супермаркеты'))
