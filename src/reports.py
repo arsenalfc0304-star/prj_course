565b1fa093134c2226876c56a11238ebc1e1d15c
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-03-16 23:32:55 +0500

    cw in progress

--- src/reports.py
+++ src/reports.py
@@ -9,14 +9,22 @@ def spending_by_category(transactions: pd.DataFrame, category: str, date: Option
     :param transactions:
     """
     if date is None:
-        date_time = datetime.datetime.now()
-    return transactions.loc[
-        transactions.Категория.isin([category])
-        & (
-            (datetime.datetime.strptime(transactions["Дата операции"], "%d.%m.%Y %H:%M:%S").month - 3)
-            >= date_time.month
-        )
-    ]
+        end_date = datetime.datetime.now()
+    else:
+        end_date = pd.to_datetime(date)
+
+    start_date = end_date - datetime.timedelta(days=90)
+
+    transactions["Дата операции"] = pd.to_datetime(transactions["Дата операции"])
+
+    mask = (
+        (transactions.Категория == category)
+        & (transactions["Дата операции"] >= start_date)
+        & (transactions["Дата операции"] <= end_date)
+    )
 
 
-print(spending_by_category(read_data_from_excel("data/operations.xlsx"), "Супермаркеты"))
+    return transactions.loc[mask]
+
+
+print(spending_by_category(read_data_from_excel("data/operations.xlsx"), "Супермаркеты", "2021-11-28 01:02:02"))
