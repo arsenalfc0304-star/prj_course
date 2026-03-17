bab55ad94326c283747b5c0eba286eb08365b3e6
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-03-17 22:49:11 +0500

    cw in progress

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ stock_apikey = os.getenv("STOCK_API_KEY")
 def select_greeting(hour_of_day: int) -> str:
     """
     принимает текущий час,
-    возвращает приветствие
+    возвращает приветствие соответственно времени суток
     """
     if 0 <= hour_of_day < 6:
         return "Доброй ночи"
@@ -30,7 +30,7 @@ def select_greeting(hour_of_day: int) -> str:
 def read_data_from_excel(path: str) -> DataFrame:
     """
     принимает путь к файлу Excel,
-    выдает список словарей с транзакциями
+    возвращает DataFrame с транзакциями
     """
     data = pd.read_excel(path)
     return data
@@ -39,20 +39,28 @@ def read_data_from_excel(path: str) -> DataFrame:
 def sort_by_amount(dict_list: list, descending: bool = True) -> list:
     """
     принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание),
-    возвращает новый список, отсортированный по дате (date)
+    возвращает новый список, отсортированный по Сумме платежа
     """
     return sorted(dict_list, key=lambda x: abs(x["Сумма платежа"]), reverse=descending)
 
 
-def get_cards(dict_list: list):
+def get_cards(transactions: list) -> set:
+    """
+    принимает список словарей с транзакциями,
+    возвращает DataFrame с транзакциями
+    """
     cards_set = set()
-    for transaction in dict_list:
+    for transaction in transactions:
         if str(transaction["Номер карты"])[1:].isdigit():
             cards_set.add(transaction["Номер карты"])
     return cards_set
 
 
-def get_card_total_expenses(transactions: list, card: str):
+def get_card_total_expenses(transactions: list, card: str) -> float:
+    """
+    принимает список словарей с транзакциями по карте,
+    возвращает значение суммарных трат по карте
+    """
     total_expenses = 0
     for transaction in transactions:
         if transaction["Номер карты"] == card and transaction["Сумма операции"] < 0:
@@ -62,7 +70,7 @@ def get_card_total_expenses(transactions: list, card: str):
 
 def get_api_currency_rate(currency: str) -> float:
     """
-    обращается к внешнему API (Exchange Rates Data API) для получения текущего курса валют
+    обращается к внешнему API (Exchange Rates Data API) для получения текущего курса валюты
     """
     url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {
@@ -82,22 +90,22 @@ def get_api_currency_rate(currency: str) -> float:
 
 def get_api_stock_price(stock: str) -> float:
     """
-    обращается к внешнему API (Exchange Rates Data API) для получения текущего курса акций
+    обращается к внешнему API (Exchange Rates Data API) для получения текущего курса акции
     """
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={stock_apikey}"
 
     response = requests.get(url)
     status_code = response.status_code
     raw_result = response.json()
-    # last_refreshed = raw_result['Meta Data']["3. Last Refreshed"]
-    # result = raw_result["Time Series (Daily)"][last_refreshed]["4. close"]
+    last_refreshed = raw_result['Meta Data']["3. Last Refreshed"]
+    result = raw_result["Time Series (Daily)"][last_refreshed]["4. close"]
     if status_code == 200:
-        return raw_result
+        return result
     else:
         return 0.0
 
 
-def load_json(path):
+def load_json(path) -> list[dict]:
     """
     принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
@@ -122,7 +130,11 @@ def sort_by_date(dict_list: list, descending: bool = False) -> list:
     )
 
 
-def format_transactions(transactions: list):
+def format_transactions(transactions: list) -> list[dict]:
+    """
+    принимает список словарей с транзакциями,
+    возвращает отформатированный список словарей с транзакциями
+    """
     formatted_transactions = []
     for transaction in transactions:
         formatted_transactions.append({
